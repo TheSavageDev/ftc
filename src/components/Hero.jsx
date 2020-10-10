3fc0c81e59d5a6c877ae4790dd47bc7b910c947f
TheSavageDev <jason@thesavage.dev>
2020-10-10 12:52:56 -0500

    update for small screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 
 const Hero = () => (
-  <header className="container py-32 bg-white">
-    <h2 className="text-6xl text-center font-extrabold leading-tight tracking-tight text-blue text-shadow-lg">
+  <header className="container py-16 lg:py-32 bg-white">
+    <h2 className="text-4xl lg:text-6xl text-center font-extrabold leading-tight tracking-tight text-blue text-shadow-lg">
       Foldaway Trailer Company
     </h2>
-    <p className="text-4xl mt-32 text-center font-extrabold leading-tight tracking-tight text-black">
+    <p className="text-2xl mt-10 lg:text-4xl lg:mt-32 text-center font-extrabold leading-tight tracking-tight text-black">
       Welcome to the new way to haul.
     </p>
   </header>
