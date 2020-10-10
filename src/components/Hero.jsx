4472cf5b044c7fb5d0d90513c6fdfa4704e43e4b
Jason A. Savage <67115428+TheSavageDev@users.noreply.github.com>
2020-10-10 13:03:14 -0500

    Dev (#3)
    * new carousel
    
    * made hero bigger
    
    * update for small screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 
 const Hero = () => (
-  <header className="container py-5">
-    <h2 className="text-3xl text-center font-extrabold leading-tight tracking-tight text-blue">
+  <header className="container py-16 lg:py-32 bg-white">
+    <h2 className="text-4xl lg:text-6xl text-center font-extrabold leading-tight tracking-tight text-blue text-shadow-lg">
       Foldaway Trailer Company
-      <br />
-      <span className="text-teal">
-        Welcome to the new way to haul.
-      </span>
     </h2>
+    <p className="text-2xl mt-10 lg:text-4xl lg:mt-32 text-center font-extrabold leading-tight tracking-tight text-black">
+      Welcome to the new way to haul.
+    </p>
   </header>
 )
 
